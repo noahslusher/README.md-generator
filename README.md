441341dd678943e05e130a88126f1c2fa751b3b0
Carl Wheezer <slushern42@gmail.com>
2022-03-24 19:17:09 -0600

    let all question prompts be asked for sake of time

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # asdf
   ![License](https://img.shields.io/badge/license-mit-blue.svg)
   ## Description
-  adsf
+  asdf
 
   ## Live URL
-  https://fa.github.io/sdf/
+  https://asdf.github.io/asdf/
 
   ## Table of Contents
 * [Installation](#Installation)
+* [Usage](#Usage)
+* [Credits](#Credits)
 * [Licenses](#Licenses)
+* [Badge](#Badge)
 * [Features](#Features)
-* [Contribution](#Contribution)
-* [Tests](#Tests)
 
 
 ## Installation
@@ -24,16 +25,20 @@
   None
   
 ## License
-  Copyright aasd [fa]
+  Copyright asdf [asdf]
   
 ## Badge
   None
   
-
-
-
+## Features
+  None
+  
+## Contribution
+  If you would like to contribute please add repo to your GitHub account and create a pull request
+  
+## Tests
+  npm test
+  
 ## Questions
-  Profile Link: https://github.com/fa
-  For any questions or concerns, please contact asd with any questions.
-
-
+  Profile Link: https://github.com/asdf
+  For any questions or concerns, please contact asdf with any questions.
--- index.js
+++ index.js
@@ -33,104 +33,104 @@ const questions = [
     name: 'installation',
     message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running',
     default: 'npm i',
-    when: ({contents}) => {
-      if (contents[0]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[0]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'input',
     name: 'usage',
     message: 'Provide instructions and examples for use. If needed, add screenshots by adding the images to an "assets/images" folder in your repository and upload the photo. Use a relative filepath to add it to your README using the syntax "```md![alt text](assets/images/screenshot.png)```"',
     default: 'Please contact me',
-    when: ({contents}) => {
-      if (contents[1]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[1]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'input',
     name: 'credits',
     message: 'List any collaborators with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.',
     default: 'None',
-    when: ({contents}) => {
-      if (contents[2]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[2]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'list',
     name: 'license',
     message: 'Choose a license if you would like. refer to "[https://choosealicense.com/](https://choosealicense.com/)" for help.',
     choices: ['mit', 'apache-2.0', 'isc', 'gpl-3.0', 'none'],
-    when: ({contents}) => {
-      if (contents[3]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[3]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'input',
     name: 'badge',
     message: 'Please add the badge you would like to use',
     default: 'None',
-    when: ({contents}) => {
-      if (contents[4]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[4]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'input',
     name: 'features',
     message: 'List your features here if you would like to.',
     default: 'None',
-    when: ({contents}) => {
-      if (contents[5]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[5]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'input',
     name: 'contribution',
     message: 'If you would like others to contribute, please leave guidelines here for how to do so.',
     default: 'If you would like to contribute please add repo to your GitHub account and create a pull request',
-    when: ({contents}) => {
-      if (contents[6]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[6]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'input',
     name: 'tests',
     message: 'Go the extra mile and write tests for your application. Then provide them here.',
     default: 'npm test',
-    when: ({contents}) => {
-      if (contents[7]) {
-        return true
-      } else {
-        return false
-      }
-    }
+    // when: ({contents}) => {
+    //   if (contents[7]) {
+    //     return true
+    //   } else {
+    //     return false
+    //   }
+    // }
   },
   {
     type: 'input',
@@ -185,7 +185,6 @@ const questions = [
     }
   },
 ]
-
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
